38ec7e530972ba7863f884efc898e494b4f3e667
Cedric <cedricao10@hotmail.com>
2020-07-06 08:52:38 -0400

    lazy loading few routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import Layout from "./hoc/Layout/Layout";
 import IcecreamBuilder from './Containers/IcecremBuilder/IcecreamBuilder';
-import Checkout from './Containers/Checkout/Checkout';
+import asyncComp from './hoc/asyncComp/asyncComp';
+// import Checkout from './Containers/Checkout/Checkout';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
-import Orders from './Containers/Orders/Orders';
-import Auth from './Containers/Auth/Auth';
+// import Orders from './Containers/Orders/Orders';
+// import Auth from './Containers/Auth/Auth';
 import Logout from './Containers/Logout/Logout';
 import * as actions from './store/actions/index';
 
+
+const asyncCheckout = asyncComp(() => {
+  return import('./Containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComp(() => {
+  return import('./Containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComp(() => {
+  return import('./Containers/Auth/Auth');
+});
 
 class App extends Component {
 
@@ -19,7 +32,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Route exact path="/" component={IcecreamBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -28,10 +41,10 @@ class App extends Component {
     if (this.props.isAuth) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Route exact path="/" component={IcecreamBuilder} />
           <Redirect to="/" />
         </Switch>
